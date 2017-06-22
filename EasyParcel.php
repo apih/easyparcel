7bcf558e0f8fcaf49333107a8ced64cd0607b98c
Mohd Hafizuddin M Marzuki <hafizuddin_83@yahoo.com>
2017-06-22 15:14:01 +0800

    Initial commit

---
+++ EasyParcel.php
@@ -0,0 +1,126 @@
+<?php
+// A wrapper for EasyParcel Marketplace API
+// Based on EasyParcel Marketplace API Documentation v2.0.0
+
+namespace apih\EasyParcel;
+
+class EasyParcel
+{
+	const DEMO_URL = 'http://demo.connect.easyparcel.my/?ac=';
+	const LIVE_URL = 'http://connect.easyparcel.my/?ac=';
+
+	protected $api_key;
+	protected $auth_key;
+	protected $url;
+
+	public function __construct($api_key, $auth_key)
+	{
+		$this->api_key = $api_key;
+		$this->auth_key = $auth_key;
+		$this->url = self::LIVE_URL;
+	}
+
+	public function useDemo($flag = true)
+	{
+		if ($flag) {
+			$this->url = self::DEMO_URL;
+		} else {
+			$this->url = self::LIVE_URL;
+		}
+	}
+
+	protected function curlRequest($action, $query_data)
+	{
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+		curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($query_data));
+		curl_setopt($ch, CURLOPT_URL, $this->url . $action);
+
+		$response = curl_exec($ch);
+
+		return json_decode($response, true);
+	}
+
+	public function checkRate($bulk)
+	{
+		$query_data = [
+			'api' => $this->api_key,
+			'authentication' => $this->auth_key,
+			'bulk' => $bulk
+		];
+
+		return $this->curlRequest('MPRateCheckingBulk', $query_data);
+	}
+
+	public function checkNormalRate($bulk)
+	{
+		$query_data = [
+			'api' => $this->api_key,
+			'authentication' => $this->auth_key,
+			'bulk' => $bulk
+		];
+
+		return $this->curlRequest('MPNormalRateCheckingBulk', $query_data);
+	}
+
+	public function submitOrder($bulk)
+	{
+		$query_data = [
+			'api' => $this->api_key,
+			'authentication' => $this->auth_key,
+			'bulk' => $bulk
+		];
+
+		return $this->curlRequest('MPSubmitOrderBulk', $query_data);
+	}
+
+	public function makePayment($bulk)
+	{
+		$query_data = [
+			'api' => $this->api_key,
+			'authentication' => $this->auth_key,
+			'bulk' => $bulk
+		];
+
+		return $this->curlRequest('MPPayOrderBulk', $query_data);
+	}
+
+	public function getOrderStatus($bulk)
+	{
+		$query_data = [
+			'api' => $this->api_key,
+			'authentication' => $this->auth_key,
+			'bulk' => $bulk
+		];
+
+		return $this->curlRequest('MPOrderStatusBulk', $query_data);
+	}
+
+	public function getParcelStatus($bulk)
+	{
+		$query_data = [
+			'api' => $this->api_key,
+			'authentication' => $this->auth_key,
+			'bulk' => $bulk
+		];
+
+		return $this->curlRequest('MPParcelStatusBulk', $query_data);
+	}
+
+	public function trackParcel($bulk)
+	{
+		$query_data = [
+			'api' => $this->api_key,
+			'authentication' => $this->auth_key,
+			'bulk' => $bulk
+		];
+
+		return $this->curlRequest('MPTrackingBulk', $query_data);
+	}
+}
+?>
